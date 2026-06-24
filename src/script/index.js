768c66aed6ecbef8b13a7d884fc3ad4acf59a442
Arjit <arjit@joshtechnologygroup.com>
2026-06-24 19:16:18 +0530

    [A_A1_04] : Travel point section

--- src/script/index.js
+++ src/script/index.js
@@ -4,3 +4,20 @@ const navbarRight = document.querySelector('.navbar__right');
 hamburger.addEventListener('click', () => {
     navbarRight.classList.toggle('navbar--active');
 });
+
+const travelData = [
+    { number: '500+', title: 'Holiday Package' },
+    { number: '100', title: 'Luxury Hotel' },
+    { number: '7', title: 'Premium Airlines' },
+    { number: '2k+', title: 'Happy Customer' },
+];
+
+const cardsContainer = document.querySelector('.travel-cards');
+
+travelData.forEach((item) => {
+    cardsContainer.innerHTML += `
+    <div class='travel-card'>
+        <h2>${item.number}</h2>
+        <p>${item.title}</p>
+    </div>`;
+});
